7f96da06a62542c347be82e1a3976fbe2cc5035b
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2020-11-21 22:55:34 +0300

    removed styles from logout page

--- next-client/pages/logout.jsx
+++ next-client/pages/logout.jsx
@@ -1,22 +1,11 @@
 import { useEffect, useContext } from 'react';
-import { makeStyles } from '@material-ui/core';
 import Router from 'next/router';
 
 import { AuthContext } from '../context/AuthContext';
 import { Redirection } from '../components/Redirection/Redirection';
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    paddingTop: theme.spacing(8),
-    paddingRight: theme.spacing(4),
-    paddingBottom: theme.spacing(8),
-    paddingLeft: theme.spacing(4),
-  }
-}));
-
 const LogoutPage = () => {
   const { logout } = useContext(AuthContext);
-  const classes = useStyles();
 
   useEffect(() => {
     logout();
